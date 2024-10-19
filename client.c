4d8d7cbdcf6db7adf0fd698ca1a82feddf5a16e4
chrisramrothh <116681173+chrisramrothh@users.noreply.github.com>
2024-10-19 16:34:31 -0400

    update client.c for function 1

--- client.c
+++ client.c
@@ -5,6 +5,7 @@
 #include <unistd.h>       /* for close() */
 #include <string.h>       /* support any string ops */
 #include <openssl/evp.h>  /* for OpenSSL EVP digest libraries/SHA256 */
+#include <dirent.h>
 
 int main(){
         char *ip = "127.0.0.1";
@@ -46,22 +47,39 @@ int main(){
 
         // LOOP WHILE INPUT IS NOT 4
         // CLIENT WILL SEND "LEAVE" WHEN INPUT IS 4
-        while (strcmp(buffer, "LEAVE") !=0)
+        while (strcmp(buffer, "LEAVE\n") !=0)
         {
+                // INPUT 1-4
                 char input[2];
                 scanf("%s", input);
 
-
+                // SEND INPUT SELECTION
                 bzero(buffer, 1024);
                 strcpy(buffer, input);
-                printf("ClientTEST: %s\n", buffer);
+                printf("Client Input: %s\n\n", buffer);
                 send(sock, buffer, strlen(buffer), 0);
 
+                // IF 1. LIST IS CHOSEN
+                if (strcmp(input, "1") == 0)
+                {
+                        // RECIEVE FILE LIST
+                        bzero(buffer, 1024);
+                        recv(sock, buffer, sizeof(buffer), 0);
+                        printf("Server: \n%s", buffer);
+
+                        // CONFIRM RECIEVED
+                        bzero(buffer, 1024);
+                        strcpy(buffer, "Recieved File List Confirmation.\n");
+                        printf("Client: %s\n", buffer);
+                        send(sock, buffer, strlen(buffer), 0);
+                }
+
+                // RECIEVE READY FOR INPUT
                 bzero(buffer, 1024);
                 recv(sock, buffer, sizeof(buffer), 0);
                 printf("Server: %s\n", buffer);
         }
-        
+
         // CLIENT HAS SENT "LEAVE", DISCONNECT
         close(sock);
         printf("Disconnected from the server.\n");
